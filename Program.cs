411653cd832a61baad330e3a502b8bdbb0a62bd4
Bryan Hadlock <bhadlock@agmonitoring.com>
2025-04-27 12:50:04 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) AvantGuard Monitoring Centers. All rights reserved.
+
+using ConvertJournalCSV;
+
+args ??= [];
+
+using var reader = new StreamReader(@"C:\Users\bhadlock\Downloads\Trade_Export.csv");
+
+var trades = new List<Trade>();
+
+var count = 0;
+while (!reader.EndOfStream)
+{
+    var line = reader.ReadLine();
+    if (count > 0)
+    {
+        var values = line.Split(',');
+
+        var time = values[0].Trim('"').Split(";");
+
+        var trade = new Trade
+        {
+            Time = TimeOnly.Parse(time[1]),
+            TimeString = time[1],
+            Symbol = values[1].Trim('"'),
+            BuySell = values[2].Trim('"'),
+            Price = values[3].Trim('"'),
+            Quantity = values[4].Trim('"'),
+            Account = values[5].Trim('"'),
+        };
+        trades.Add(trade);
+    }
+    count++;
+}
+
+var sortedTrades = trades.OrderBy(x => x.Time).ToList();
+var tradeStack = new List<TradeSide>();
+
+using var sw = new StreamWriter(@"C:\Users\bhadlock\Downloads\trade_converted.csv");
+sw.WriteLine("Time,Symbol,Side,Price,Qty,Route,Broker,Account,Type,Cloid");
+foreach (var trade in sortedTrades)
+{
+
+    var tradeSide = tradeStack.SingleOrDefault(x=> x.Symbol == trade.Symbol);
+    var quantity = GetQuanity(trade);
+    if (tradeSide == null)
+    {
+        
+        var tradeSidedd = new TradeSide
+        {
+            Quanity = quantity,
+            Symbol = trade.Symbol,
+        };
+        if (trade.BuySell == "SELL")
+        {
+            tradeSidedd.Side = Side.Short;
+            sw.WriteLine($"{trade.TimeString},{trade.Symbol},SS,{trade.Price},{quantity},SMRT,BEST,U8490678,Short,1");
+        }
+        else
+        {
+            tradeSidedd.Side = Side.Long;
+            sw.WriteLine($"{trade.TimeString},{trade.Symbol},B,{trade.Price},{quantity},SMRT,BEST,U8490678,Margin,1");
+        }
+        tradeStack.Add(tradeSidedd);
+    }
+    else
+    {
+        if (tradeSide.Side == Side.Short && trade.BuySell == "SELL")
+        {
+            tradeSide.Quanity = tradeSide.Quanity + quantity;
+            sw.WriteLine($"{trade.TimeString},{trade.Symbol},SS,{trade.Price},{quantity},SMRT,BEST,U8490678,Short,1");
+        }
+        else if (tradeSide.Side == Side.Long && trade.BuySell == "BUY")
+        {
+            tradeSide.Quanity = tradeSide.Quanity + quantity;
+            sw.WriteLine($"{trade.TimeString},{trade.Symbol},B,{trade.Price},{quantity},SMRT,BEST,U8490678,Margin,1");
+        }
+        else
+        {
+            if (trade.BuySell == "SELL")
+            {
+                sw.WriteLine($"{trade.TimeString},{trade.Symbol},S,{trade.Price},{quantity},SMRT,BEST,U8490678,Margin,1");
+            }
+
+            if (trade.BuySell == "BUY") 
+            {
+                sw.WriteLine($"{trade.TimeString},{trade.Symbol},B,{trade.Price},{quantity},SMRT,BEST,U8490678,Margin,1");
+            }
+            tradeSide.Quanity = tradeSide.Quanity - quantity;
+
+            if (tradeSide.Quanity == 0)
+            {
+                tradeStack.Remove(tradeSide);
+            }
+        }
+    }
+}
+
+static int GetQuanity(Trade? trade)
+{
+    return int.Parse(trade.Quantity) > 0 ? int.Parse(trade.Quantity) : int.Parse(trade.Quantity) * -1;
+}
---
+++ Trade.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertJournalCSV
+{
+    internal class Trade
+    {
+        public TimeOnly Time { get; set; }
+        public string? TimeString { get; set; }
+        public string? Symbol { get; set; }
+        public string? BuySell { get; set; }
+        public string? Price { get; set; }
+        public string? Quantity { get; set; }
+        public string? Account { get; set; }
+    }
+
+
+    internal class TradeSide
+    {
+        public string? Symbol { get; set; }
+        public Side Side { get; set; }
+        public int Quanity { get; set; }
+
+    }
+
+    internal enum Side {
+        Short,
+        Long,
+    }
+}
